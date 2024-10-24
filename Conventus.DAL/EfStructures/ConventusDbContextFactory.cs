9295726c3696c44e195203b42c7fa90f7df02ea3
hiraethese <hiraethese@2527283553.kn.vutbr.cz>
2024-10-25 01:04:20 +0200

    Working project on Linux Fedora

--- Conventus.DAL/EfStructures/ConventusDbContextFactory.cs
+++ Conventus.DAL/EfStructures/ConventusDbContextFactory.cs
@@ -10,7 +10,7 @@ namespace Conventus.DAL.EfStructures
         {
             var optionsBuilder = new DbContextOptionsBuilder<ConventusDbContext>();
             // Change To Your Server Name (I use my local custom)
-            var connectionString = "Server=VLAD;Database=Conventus;Trusted_Connection=True;TrustServerCertificate=True";
+            var connectionString = "Server=localhost;Database=Conventus;User Id=sa;Password=NewSecurePassword123!;TrustServerCertificate=True";
             optionsBuilder.UseSqlServer(connectionString);
             Console.WriteLine(connectionString);
             return new ConventusDbContext(optionsBuilder.Options);
