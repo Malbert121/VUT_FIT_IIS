d37e81089fb3eb21f265f189b4ff723918b68611
SmeysyaHromche <MyrKykhta@gmail.com>
2024-11-14 22:31:22 +0100

    boolsheat beasness logic for presentations

--- Conventus.DAL/Repositories/PresentationRepo.cs
+++ Conventus.DAL/Repositories/PresentationRepo.cs
@@ -14,5 +14,10 @@ namespace Conventus.DAL.Repositories
         internal PresentationRepo(DbContextOptions<ConventusDbContext> options) : base(options)
         {
         }
+
+        public virtual User? GetUser(int id)
+        => Context.Users.SingleOrDefault(u => u.Id == id);
+        public virtual Conference? GetConference(int id)
+        => Context.Conferences.SingleOrDefault(u => u.Id == id);
     }
 }
