3c5cc66d44fe00c87eef0d98a9a55c0184b5c343
SmeysyaHromche <MyrKykhta@gmail.com>
2024-11-17 15:36:51 +0100

    change logic for crud action in reservations detail page

--- conventus-client/src/Components/ReservationDetailCard/ReservationDetailCard.tsx
+++ conventus-client/src/Components/ReservationDetailCard/ReservationDetailCard.tsx
@@ -1,86 +1,27 @@
 import React from 'react';
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import { Reservation } from '../../data';
-import { pathConferences, pathMyReservations } from '../../Routes/Routes';
-import { putResirvationsToConfirm, putResirvationsToPay, deleteReservations } from '../../api';
+import { pathConferences } from '../../Routes/Routes';
 import { useUser } from '../../context/UserContext';
-import Toast from '../../Components/Toast/Toast';
-
 
 interface Props{
-    reservation: Reservation
-}
+    reservation: Reservation,
+    toPaid: ()=>void,
+    toConfirm: (flag: boolean)=>void,
+    toDelete: ()=>void,
+};
 
-const ReservationDetailCard: React.FC<Props> = ({reservation}) => 
+const ReservationDetailCard: React.FC<Props> = ({reservation, toPaid, toConfirm, toDelete}) => 
 {
     const [statusText, statusColor] = !reservation.IsPaid 
     ? ['Unpaid', 'red'] 
     : reservation.IsConfirmed 
         ? ['Confirmed', 'green'] 
         : ['Unconfirmed', 'yellow'];
-    const navigate = useNavigate();
     const user = useUser();
-    const [toastMessage, setToastMessage] = useState<string | null>(null);
-    const [toastType, setToastType] = useState<'success' | 'error' | 'info'>('info');
-    const closeToast = () => setToastMessage(null);
-
-    const handleReservationsToPayment = async ()=>{
-        try
-        {
-          await putResirvationsToPay([reservation.Id], Number(user?.id));
-          navigate(0);
-        }
-        catch(error)
-        {
-          console.error(error);
-          setToastType('error');
-          setToastMessage((error as Error).message);
-        }
-    };
-    const handleReservationsToConfirm = async (flag:boolean)=>{
-        try
-        {
-          await putResirvationsToConfirm([reservation.Id], flag);
-          navigate(0);
-        }
-        catch(error)
-        {
-          console.error(error);
-          setToastType('error');
-          setToastMessage((error as Error).message);
-        }
-    };
-    
-      const handleReservationsToDelete = async ()=>{
-        try
-        {
-          if(user)
-          {
-            await deleteReservations([reservation.Id], Number(user.id));
-            navigate(`../${pathMyReservations}`);
-          }
-          else
-          {
-            console.log('Unauthorized user is bad boy!'); //TODO: solve unauthorized user behavioral  
-            setToastType('error');
-            setToastMessage('Unauthorized user is bad boy!');
-          }
-        }
-        catch(error)
-        {
-          console.error(error);
-          setToastType('error');
-          setToastMessage((error as Error).message);
-        }
-    };
-    
 
     return (
         <>
-        {toastMessage && (
-        <Toast message={toastMessage} onClose={closeToast} type={toastType} />
-        )}
         {(user?.role === 'Admin' || Number(user?.id) === reservation.Conference?.OrganizerId  || Number(user?.id) === reservation.UserId) &&(
         <div className='flex flex-col max-w-[800px] mx-auto p-5 border border-gray-300 rounded-lg shadow-md bg-white-100'>
             <h1 className='text-center text-2xl font-semibold'>Reservation #{reservation.Id}</h1>
@@ -100,17 +41,17 @@ const ReservationDetailCard: React.FC<Props> = ({reservation}) =>
                     reservation.IsPaid ?(
                         <>
                             <button          
-                            onClick={() => { handleReservationsToConfirm(true); }}
+                            onClick={()=>{toConfirm(true)}}
                             className="bg-green-500 text-white w-32 py-2 px-4 rounded hover:bg-green-600 transition-colors duration-150">
                                 Confirm
                             </button>
                             <button          
-                            onClick={() => { handleReservationsToConfirm(false); }}
+                            onClick={()=>{toConfirm(false)}}
                             className="bg-yellow-500 text-white w-32 flex-1 py-2 px-4 rounded hover:bg-red-600 transition-colors duration-150">
                                 Unconfirm
                             </button>
                             <button           
-                            onClick={() => { handleReservationsToDelete(); }}
+                            onClick={toDelete}
                             className="bg-red-500 text-white w-32 flex-1 py-2 px-4 rounded hover:bg-red-600 transition-colors duration-150">
                                 Delete
                             </button>
@@ -119,12 +60,12 @@ const ReservationDetailCard: React.FC<Props> = ({reservation}) =>
                     (
                         <>
                         <button           
-                        onClick={() => { handleReservationsToPayment(); }}
+                        onClick={toPaid}
                         className="bg-green-500 text-white w-32 py-2 px-4 rounded hover:bg-green-600 transition-colors duration-150">
                             Paid
                         </button>
                         <button           
-                        onClick={() => { handleReservationsToDelete(); }}
+                        onClick={toDelete}
                         className="bg-red-500 text-white w-32 flex-1 py-2 px-4 rounded hover:bg-red-600 transition-colors duration-150">
                             Delete
                         </button>
@@ -134,17 +75,17 @@ const ReservationDetailCard: React.FC<Props> = ({reservation}) =>
                 ): Number(user?.id) === reservation.Conference?.OrganizerId ? (
                     <>
                         <button          
-                        onClick={() => { handleReservationsToConfirm(true); }}
+                        onClick={()=>{toConfirm(true)}}
                         className="bg-green-500 text-white w-32 py-2 px-4 rounded hover:bg-green-600 transition-colors duration-150">
                             Confirm
                         </button>
                         <button          
-                        onClick={() => { handleReservationsToConfirm(false); }}
+                        onClick={() => { toConfirm(false); }}
                         className="bg-yellow-500 text-white w-32 flex-1 py-2 px-4 rounded hover:bg-red-600 transition-colors duration-150">
                             Unconfirm
                         </button>
                         <button           
-                        onClick={() => { handleReservationsToDelete(); }}
+                        onClick={toDelete}
                         className="bg-red-500 text-white w-32 flex-1 py-2 px-4 rounded hover:bg-red-600 transition-colors duration-150">
                             Delete
                         </button>
@@ -152,19 +93,19 @@ const ReservationDetailCard: React.FC<Props> = ({reservation}) =>
                 ) : !reservation.IsPaid ? (
                     <>
                         <button           
-                        onClick={() => { handleReservationsToPayment(); }}
+                        onClick={toPaid}
                         className="bg-green-500 text-white w-32 py-2 px-4 rounded hover:bg-green-600 transition-colors duration-150">
                             Paid
                         </button>
                         <button           
-                        onClick={() => { handleReservationsToDelete(); }}
+                        onClick={toDelete}
                         className="bg-red-500 text-white w-32 flex-1 py-2 px-4 rounded hover:bg-red-600 transition-colors duration-150">
                             Delete
                         </button>
                     </>
                 ) : (
                     <button           
-                    onClick={() => { handleReservationsToDelete(); }}
+                    onClick={toDelete}
                     className="bg-red-500 text-white w-32 flex-1 py-2 px-4 rounded hover:bg-red-600 transition-colors duration-150">
                         Delete
                     </button>
--- conventus-client/src/Pages/ReservationDetailPage/ReservationDetailPage.tsx
+++ conventus-client/src/Pages/ReservationDetailPage/ReservationDetailPage.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useParams} from 'react-router-dom';
 import { Reservation } from '../../data';
 import { getReservation } from '../../api';
 import ReservationDetailCard from '../../Components/ReservationDetailCard/ReservationDetailCard';
+import { putResirvationsToConfirm, putResirvationsToPay, deleteReservations } from '../../api';
 import { useUser } from '../../context/UserContext'
+import Toast from '../../Components/Toast/Toast';
+
 interface Props{}
 
 const ReservationDetailPage: React.FC<Props> = () => {
@@ -13,34 +16,32 @@ const ReservationDetailPage: React.FC<Props> = () => {
     const [reservation, setReservation] = useState<Reservation | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
-    
- 
+    const [toastMessage, setToastMessage] = useState<string | null>(null);
+    const [toastType, setToastType] = useState<'success' | 'error' | 'info'>('info');
+    const closeToast = () => setToastMessage(null);
+
+    const fetchReservation = useCallback(async ()=>{
+        if(!user){
+            return;
+        }
+        console.log("Fetching reservation with ID:", reservationId);
+        setIsAuthorized(true);
+        try{
+            const data = await getReservation(Number(reservationId), Number(user.id));
+            setReservation(data);
+        }
+        catch(error){
+            setError('Failed to fetch reservation details.');
+            console.error("Error fetching reservation details:", error);
+        }
+        finally{
+            setLoading(false);
+        }
+    }, [user, reservationId]);
+
     useEffect(()=>{
-        const fetchReservation = async ()=>
-        {
-            if(!user)
-            {
-                return;
-            }
-            console.log("Fetching reservation with ID:", reservationId);
-            setIsAuthorized(true);
-            try
-            {
-                const data = await getReservation(Number(reservationId), Number(user.id));
-                setReservation(data);
-            }
-            catch(error)
-            {
-                setError('Failed to fetch reservation details.');
-                console.error("Error fetching reservation details:", error);
-            }
-            finally
-            {
-                setLoading(false);
-            }
-        };
         fetchReservation();
-    }, [reservationId, user])
+    }, [fetchReservation])
     
     if(!isAuthorized){
         return <div className="error">User should be authorized for interaction with reservations.</div>;
@@ -55,9 +56,67 @@ const ReservationDetailPage: React.FC<Props> = () => {
     if (!reservation) {
         return <div className="error">Reservation not found.</div>;
     }
+
+    const handleReservationsToPayment = async ()=>{
+        try{
+            await putResirvationsToPay([reservation.Id], Number(user?.id));
+            setToastType("success");
+            setToastMessage("User have successfully paid reservation.");
+            fetchReservation();
+        }
+        catch(error){
+            console.error(error);
+            setToastType('error');
+            setToastMessage((error as Error).message);
+        }
+    };
+
+    const handleReservationsToConfirm = async (flag:boolean)=>{
+        try{
+            await putResirvationsToConfirm([reservation.Id], flag);
+            setToastType("success");
+            setToastMessage("User have successfully confirmed reservation.");
+            fetchReservation();
+        }
+        catch(error){
+            console.error(error);
+            setToastType('error');
+            setToastMessage((error as Error).message);
+        }
+    };
     
+    const handleReservationsToDelete = async ()=>{
+    try{
+        if(user){
+            await deleteReservations([reservation.Id], Number(user.id));
+            setToastType("success");
+            setToastMessage("User have successfully deleted reservation.");
+            fetchReservation();
+        }
+        else{
+            console.log('Unauthorized user is bad boy!'); //TODO: solve unauthorized user behavioral  
+            setToastType('error');
+            setToastMessage('Unauthorized user is bad boy!');
+        }
+    }
+    catch(error){
+        console.error(error);
+        setToastType('error');
+        setToastMessage((error as Error).message);
+    }
+};
     return (
-        <ReservationDetailCard reservation={reservation}/>
+        <>
+            {toastMessage && (
+                <Toast message={toastMessage} onClose={closeToast} type={toastType} />
+            )}
+            <ReservationDetailCard 
+                reservation={reservation}
+                toPaid={()=>{handleReservationsToPayment()}}
+                toConfirm={(flag:boolean)=>{handleReservationsToConfirm(flag)}}
+                toDelete={()=>{handleReservationsToDelete()}}
+            />
+        </>
     );
 }
 export default ReservationDetailPage
