53982441bb56e7a9013fa9ae92267cf71e048135
Malashchuk Vladyslav <xmalas04@vutbr.cz>
2024-11-21 19:17:58 +0100

    fix: naming of button

--- conventus-client/src/Pages/ConferencesPage/ConferencesPage.tsx
+++ conventus-client/src/Pages/ConferencesPage/ConferencesPage.tsx
@@ -271,7 +271,7 @@ function ConferencesPage() {
                   handleQuantityChange(conference.Id, newQuantity);
                   handleCreateReservation(conference.Id, newQuantity)}}
                 className="ticket-button bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors duration-200 shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
-                Add to Cart
+                Reserve Ticket
               </button>
             </div>
 
